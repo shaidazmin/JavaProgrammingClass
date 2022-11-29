945cb775f06e90423e60e2289e9aa5770cc751b2
shaidazmin <shaidazmin@gmail.com>
2022-11-29 22:11:28 +0600

    variable, data type, oparater, oparend etc

---
+++ src/learnjava/HelloJava.java
@@ -0,0 +1,88 @@
+package learnjava;
+
+import java.util.Scanner;
+
+public class HelloJava {
+
+
+	public static void main(String args[]){
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		int a = 5;
+		int b = 7;
+		
+		
+		System.out.println("AND "+(a & b));
+		System.out.println("OR "+(a | b));
+		System.out.println("XOR "+(a ^ b));
+		System.out.println("Complement "+(~a));
+		
+		
+//		int a = 5;
+//		System.out.println("first number "+a);
+//		
+//		int value = a>>2;
+//		
+//		int phy = 90;
+//		
+//		if(phy == 90) {
+//			// .............
+//		}
+//		
+//		System.out.println("second number "+value);
+		
+		
+		
+		
+//		String userName;
+////		userName = input.nextLine();
+//		
+//		int numberOne, numbertwo;
+//		float average;
+//		
+//		
+//		System.out.println("first number ");
+//		numberOne = input.nextInt();
+//		
+//		
+//		System.out.println("first number is "+numberOne);
+		
+//		System.out.println("second number ");
+//		numbertwo = input.nextInt();
+//		
+//		average = (numberOne + numbertwo) / 2;
+//		
+//		
+//		System.out.println("Average  "+average);
+		
+		
+		
+//		boolean ssc_Result, areYouHonest;
+//		
+//		ssc_Result = false;
+//		areYouHonest = true;
+//		
+//		// a-z | A - Z  
+//		char choseKey = 'A'; 
+//		
+//		// 0 - 9
+//		 
+////		byte, short, int, long 
+//		
+//		long number = 10;
+//		
+//		// float .... 2234.78783
+//		
+//		//float, double
+//		
+//		double numberOfAverage = (float) 69.33;
+//		
+//		
+//		System.out.print(choseKey);
+//		int data=50;//instance variable
+	}
+
+}
+
